03e38491eb0da56fcd4eeb0e4ecf64751ca77e13
nathankang413 <nathankang413@gmail.com>
2024-02-08 11:42:00 -0800

    Set up REST api structure

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -5,3 +5,40 @@ const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+app.post("/flocks", (req, res) => {
+	const code = "flyingtaco";
+	res.send(`Flock created with code: ${code}`);
+});
+
+app.get("/flocks/:code", (req, res) => {
+	res.send(`Flock ${req.params.code}`);
+});
+
+app.delete("/flocks/:code", (req, res) => {
+	res.send(`Flock ${req.params.code} deleted`);
+});
+
+app.get("/flocks/:code/chicks", (req, res) => {
+	res.send(`Chicks of flock ${req.params.code}`);
+});
+
+app.post("/flocks/:code/chicks", (req, res) => {
+	res.send(`Chick added to flock ${req.params.code}`);
+});
+
+app.post("/flocks/:code/options", (req, res) => {
+	res.send(`Options added to flock ${req.params.code}`);
+});
+
+app.get("/flocks/:code/options", (req, res) => {
+	res.send(`Options of flock ${req.params.code}`);
+});
+
+app.get("/flocks/:code/decision", (req, res) => {
+	res.send(`Decision of flock ${req.params.code}`);
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});

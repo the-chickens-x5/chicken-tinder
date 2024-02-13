3335b97cbd22e95afc37638082f6062c228c56e9
PhysBoom <physboomtechdevelopment@gmail.com>
2024-02-13 13:19:57 -0800

    Linked welcome page to work w/ group creation backend stuff

--- packages/frontend/src/pages/WelcomePage.js
+++ packages/frontend/src/pages/WelcomePage.js
@@ -2,15 +2,24 @@ import React from "react";
 import { FullWidthButton } from "../components/Input/Buttons";
 import TextButtonInput from "../components/Input/TextButtonInput";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function WelcomePage() {
+	const navigate = useNavigate();
+
 	async function createGroup() {
 		const res = await axios.post(`${process.env.REACT_APP_API_URL}/flocks`);
+		if (res.status === 201) {
+			console.log("Group created",  res.data);
+			navigate(`/flock/${res.data.coop_name}/join`);
+		} else {
+			console.error("Failed to create group");
+		}
 	}
 
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
-			<FullWidthButton onClick={() => console.log("Creating group")}>
+			<FullWidthButton onClick={createGroup}>
 				Rally my flock
 			</FullWidthButton>
 			<TextButtonInput

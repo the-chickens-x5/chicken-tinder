19227b6c053aeb4c448435866d2d6a8965744cfa
PhysBoom <physboomtechdevelopment@gmail.com>
2024-02-15 13:04:23 -0800

    Added toasting

--- packages/frontend/src/pages/GroupListPage.js
+++ packages/frontend/src/pages/GroupListPage.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { FullWidthText } from "../components/Input/Text";
-import { CopyTextButton, SmallButton } from "../components/Input/Buttons";
+import { SmallButton } from "../components/Input/Buttons";
 import { BigText } from "../components/Input/Text";
 import TextButtonInput from "../components/Input/TextButtonInput";
+import toast from "react-hot-toast";
 
 export default function GroupListPage() {
 	const params = useParams();
+
+	function copyToClipboardAndNotify(input) {
+		navigator.clipboard.writeText(input);
+		toast.success("Copied to clipboard!", {
+			position: "bottom-right",
+		});
+	}
 
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
@@ -14,7 +22,7 @@ export default function GroupListPage() {
 			<TextButtonInput
 				value={`http://localhost:3000/flock/${params.coop_name}/join/`}
 				buttonText="copy invite link"
-				onClick={(input) => navigator.clipboard.writeText(input)}
+				onClick={copyToClipboardAndNotify}
 				textDisabled={true}
 			/>
 			<BigText>My Flock</BigText>

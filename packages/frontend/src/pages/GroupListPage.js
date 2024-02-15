1a5c30f12719146e3df91d7b05d86317fe194369
PhysBoom <physboomtechdevelopment@gmail.com>
2024-02-15 12:55:01 -0800

    Added copy link functionality

--- packages/frontend/src/pages/GroupListPage.js
+++ packages/frontend/src/pages/GroupListPage.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { FullWidthText } from "../components/Input/Text";
 import { CopyTextButton, SmallButton } from "../components/Input/Buttons";
 import { BigText } from "../components/Input/Text";
+import TextButtonInput from "../components/Input/TextButtonInput";
 
 export default function GroupListPage() {
 	const params = useParams();
@@ -10,10 +11,11 @@ export default function GroupListPage() {
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
 			<FullWidthText>Coop Name:</FullWidthText>
-			<CopyTextButton
-				displayText={`http://localhost:3000/flock/${params.coop_name}/join/`}
+			<TextButtonInput
+				value={`http://localhost:3000/flock/${params.coop_name}/join/`}
 				buttonText="copy invite link"
-				onClick={(input) => console.log("Invite link copied to clipboard", input)}
+				onClick={(input) => navigator.clipboard.writeText(input)}
+				textDisabled={true}
 			/>
 			<BigText>My Flock</BigText>
 			<SmallButton buttonText="let's go -->" />

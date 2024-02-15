beede4dfa7aa7ced42319858386dd2b461bf96ea
nathankang413 <nathankang413@gmail.com>
2024-02-15 11:34:17 -0800

    switch to dotenv package

--- packages/backend/flock-services.js
+++ packages/backend/flock-services.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 import flockModel from "./flock.js";
+import dotenv from "dotenv";
 import process from "process";
 import codeGenerator from "./code-generation/code-generator.js";
 
+dotenv.config();
 const db_password = process.env.DB_PASSWORD;
 const url = `mongodb+srv://shareduser:${db_password}@ctcluster0.6s3myd5.mongodb.net/?retryWrites=true&w=majority`;
 

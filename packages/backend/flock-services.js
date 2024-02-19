8ce4455d6bb00c22cb54cbfe552220fab2f33707
Cece Hildreth <46549685+CHildreth297@users.noreply.github.com>
2024-02-18 22:21:07 -0800

    createEgg working

--- packages/backend/flock-services.js
+++ packages/backend/flock-services.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 import flockModel from "./flock.js";
-import eggSchema from "./flock.js";
 import process from "process";
 import codeGenerator from "./code-generation/code-generator.js";
-import Flock from "./flock.js";
 
 const db_password = process.env.DB_PASSWORD;
 const url = `mongodb+srv://shareduser:${db_password}@ctcluster0.6s3myd5.mongodb.net/?retryWrites=true&w=majority`;

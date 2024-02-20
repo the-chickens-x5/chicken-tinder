62f44edf2b360c1f3a346ff88070f6634cab6aea
nathankang413 <nathankang413@gmail.com>
2024-02-20 10:41:19 -0800

    use localStorage and useEffect for NameFormPage

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import WelcomePage from "./pages/WelcomePage";
 import NameFormPage from "./pages/NameFormPage";
 import GroupListPage from "./pages/GroupListPage";
@@ -10,26 +9,18 @@ import LoadingPage from "./pages/LoadingPage";
 import WinnerPage from "./pages/WinnerPage";
 
 function App() {
-	const [chick, setChick] = useState(null);
-
 	return (
 		<div className="flex flex-col space-y-normal w-full items-center">
 			<Header />
 			<Routes>
 				<Route exact path="/" element={<WelcomePage />} />
 				<Route path="/about" element={<div>About</div>} />
-				<Route path="/flock/:coop_name/lobby/" element={<GroupListPage chick={chick} />} />
-				<Route
-					path="/flock/:coop_name/nominations/"
-					element={<NominationPage chick={chick} />}
-				/>
-				<Route path="/flock/:coop_name/voting/" element={<VotingPage chick={chick} />} />
-				<Route path="/flock/:coop_name/loading/" element={<LoadingPage chick={chick} />} />
-				<Route path="/flock/:coop_name/winner/" element={<WinnerPage chick={chick} />} />
-				<Route
-					path="/flock/:coop_name/join/"
-					element={<NameFormPage chick={chick} setChick={setChick} />}
-				/>
+				<Route path="/flock/:coop_name/lobby/" element={<GroupListPage />} />
+				<Route path="/flock/:coop_name/nominations/" element={<NominationPage />} />
+				<Route path="/flock/:coop_name/voting/" element={<VotingPage />} />
+				<Route path="/flock/:coop_name/loading/" element={<LoadingPage />} />
+				<Route path="/flock/:coop_name/winner/" element={<WinnerPage />} />
+				<Route path="/flock/:coop_name/join/" element={<NameFormPage />} />
 			</Routes>
 		</div>
 	);

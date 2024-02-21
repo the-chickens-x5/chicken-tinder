0cc5fe0d31d12aae0a506e9b1b0e5ec98c15cdbb
Bernice Lau <bernicelau430@gmail.com>
2024-02-21 09:06:48 -0800

    decision algo, frontend linking in progress

--- packages/frontend/src/pages/WinnerPage.js
+++ packages/frontend/src/pages/WinnerPage.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FullWidthText, BigText } from "../components/Input/Text";
 import { HalfWidthButton } from "../components/Input/Buttons";
 
 export default function WinnerPage() {
+	const [winningRestaurant, setWinningRestaurant] = useState("");
+
+    useEffect(() => {
+        async function fetchWinningRestaurant() {
+            const result = await getWinningRestaurant();
+            setWinningRestaurant(result);
+        }
+        fetchWinningRestaurant();
+    }, []);
+
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
 			<FullWidthText>Winner Winner Chicken Dinner!</FullWidthText>
-			<BigText>Restaurant Name</BigText>
+			<BigText>{winningRestaurant}</BigText>
 			<div className="flex justify-between">
 				<HalfWidthButton>Return Home</HalfWidthButton>
 				<HalfWidthButton>Revote</HalfWidthButton>

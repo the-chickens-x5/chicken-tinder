4f3f4fd967321f8d4f6470b6b00720d159fbff59
Cece Hildreth <46549685+CHildreth297@users.noreply.github.com>
2024-02-27 13:17:54 -0800

    modified nomination page cuz i forgot to merge properly

--- packages/frontend/src/pages/NominationPage.js
+++ packages/frontend/src/pages/NominationPage.js
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import { FullWidthText } from "../components/Input/Text";
 import TextButtonInput from "../components/Input/TextButtonInput";
 import { BigText } from "../components/Input/Text";
 import { SmallButton } from "../components/Input/Buttons";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import Table from "../components/Table";
 
 export default function NominationPage() {
+	const navigate = useNavigate();
+	const params = useParams();
+	const [restaurants, setRestaurants] = useState([]);
+
+	async function postEggs(title) {
+		const result = await fetch(
+			`${process.env.REACT_APP_API_URL}/flocks/${params.coop_name}/basket/${title}`,
+			{ method: "POST" }
+		);
+		if (result.ok) {
+			setRestaurants((prevRestaurants) => [...prevRestaurants, title]);
+		}
+		return result;
+	}
+
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_API_URL}/flocks/${params.coop_name}/basket/`)
+			.then((response) => response.json())
+			.then((data) => setRestaurants(data))
+			.catch((error) => console.error("Error:", error));
+	}, [params.coop_name]);
+
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
 			<FullWidthText>Put your eggs in the basket</FullWidthText>
@@ -19,7 +40,11 @@ export default function NominationPage() {
 				}}
 			/>
 			<BigText>The Basket</BigText>
-			<SmallButton buttonText="let's go -->" />
+			<Table rows={restaurants} />
+			<SmallButton 
+				buttonText="let's go -->" 
+				onClick={() => navigate(`/flock/${params.coop_name}/voting/`)}
+			/>
 		</div>
 	);
 }

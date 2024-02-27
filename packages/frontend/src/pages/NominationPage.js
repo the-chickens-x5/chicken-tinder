e7d9c8bf56bde60220d7b7f9b948e1067ce7ab1f
Cece Hildreth <46549685+CHildreth297@users.noreply.github.com>
2024-02-27 13:01:38 -0800

    buttons working from GroupListPage --> Nominations --> Voting

--- packages/frontend/src/pages/NominationPage.js
+++ packages/frontend/src/pages/NominationPage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { FullWidthText } from "../components/Input/Text";
 import TextButtonInput from "../components/Input/TextButtonInput";
 import { BigText } from "../components/Input/Text";
 import { SmallButton } from "../components/Input/Buttons";
 
 export default function NominationPage() {
+	const navigate = useNavigate();
+	const params = useParams();
 	return (
 		<div className="flex flex-col space-y-normal justify-center w-5/6">
 			<FullWidthText>Put your eggs in the basket</FullWidthText>
@@ -14,7 +17,10 @@ export default function NominationPage() {
 				onClick={(input) => console.log("Restaurant name submitted", input)}
 			/>
 			<BigText>The Basket</BigText>
-			<SmallButton buttonText="let's go -->" />
+			<SmallButton 
+				buttonText="let's go -->" 
+				onClick={() => navigate(`/flock/${params.coop_name}/voting/`)}
+			/>
 		</div>
 	);
 }

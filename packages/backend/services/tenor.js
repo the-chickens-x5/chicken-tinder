0385a3bd76be62975cd2bcd343b8de2c733828d3
PhysBoom <physboomtechdevelopment@gmail.com>
2024-03-04 18:38:56 -0800

    Added process

--- packages/backend/services/tenor.js
+++ packages/backend/services/tenor.js
@@ -1,3 +1,5 @@
+import process from "process";
+
 export async function getTenorGIF(query) {
 	const url = `https://tenor.googleapis.com/v2/search?q=${query}&key=${process.env.TENOR_API_KEY}&limit=1`;
 	const response = await fetch(url);

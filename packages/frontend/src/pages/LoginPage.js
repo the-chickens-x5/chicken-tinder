569fb0c7a3c3619c3a5d5c6bff96d0a026864aea
Cece Hildreth <46549685+CHildreth297@users.noreply.github.com>
2024-03-10 15:04:34 -0700

    changed small button function of login page

--- packages/frontend/src/pages/LoginPage.js
+++ packages/frontend/src/pages/LoginPage.js
@@ -8,14 +8,14 @@ export default function LoginPage(){
     const navigate = useNavigate();
 
     createHen(){
-        
+
     }
     return (
         <div className="flex flex-col space-y-normal justify-center w-5/6">
             <form>
             <Input placeholder="user@chickentinder.com">Email</Input>
             <Input>Password</Input> 
-            <SmallButton buttonText="Submit" onClick={createHen}>
+            <SmallButton buttonText="Submit" onClick={(input) => navigate(`/welcome`)}>
 
             </SmallButton>
             

0a865394654dbd2859cab81832abd1726d33036c
Cece Hildreth <46549685+CHildreth297@users.noreply.github.com>
2024-03-10 15:29:20 -0700

    handled change for inputs on login page

--- packages/frontend/src/pages/LoginPage.js
+++ packages/frontend/src/pages/LoginPage.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { SmallButton } from "../components/Input/Buttons";
 import TextButtonInput from "../components/Input/TextButtonInput";
 import { Input } from "../components/Input/Input";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/auth-context";
 
-export default function LoginPage(){
+export default async function LoginPage(){
     const navigate = useNavigate();
+    const [emailValue, setEmail] = useState();
+    const [passValue, setPassword] = useState();
 
-    createHen(){
-
+    async function checkAuth(email, pass){
+        
     }
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleChangePass = (event) => {
+        setPassword(event.target.value);
+    }
+
     return (
         <div className="flex flex-col space-y-normal justify-center w-5/6">
             <form>
-            <Input placeholder="user@chickentinder.com">Email</Input>
-            <Input>Password</Input> 
-            <SmallButton buttonText="Submit" onClick={(input) => navigate(`/welcome`)}>
-
-            </SmallButton>
-            
+                <Input placeholder="user@chickentinder.com" onChange={handleChangeEmail}>Email</Input>
+                <Input onChange={handleChangePass}>Password</Input> 
+                <SmallButton buttonText="Submit" onClick={await checkAuth(emailValue, passValue)}></SmallButton>
             </form>
            
         </div>
